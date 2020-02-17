60bd667e0afb5f4c6bd93bbf99d58351d4cdedf8
Gravy77 <45749698+Gravy77@users.noreply.github.com>
2020-02-16 21:14:08 -0800

    Add files via upload

--- text_game_scenes.py
+++ text_game_scenes.py
@@ -2,6 +2,7 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 
+# Parent class for scenes. Includes default enter function
 class Scene(object):
 
     def enter(self):
@@ -9,21 +10,31 @@ class Scene(object):
         print("Subclass it and implement enter().")
         exit(1)
 
+# Runner class to loop through the scenes until the end
+# It takes an object argument that must be a Map
 class Engine(object):
 
     def __init__(self, scene_map):
+        # Initialize scene_map value from the Map argument
         self.scene_map = scene_map
 
     def play(self):
+        # Obtain the start_scene from the Map variable,
+        # assign it to current_scene
         current_scene = self.scene_map.opening_scene()
+        # Set the last scene
         last_scene = self.scene_map.next_scene('finished')
 
+        # Loop through scenes until the last scene
         while current_scene != last_scene:
 
+            # Assign next_scene_name to the returb result of current_scene.enter()
             next_scene_name = current_scene.enter()
+            # Obtain the class name of the next scene and assign it
+            # to current_scene
             current_scene = self.scene_map.next_scene(next_scene_name)
 
-        # be sure to print out the last scene name
+        # After the while loop, enter the final scene
         current_scene.enter()
 
 class Car(Scene):
@@ -38,6 +49,7 @@ class Car(Scene):
             It won't start!
             """))
 
+        # Get input from user, compare to common answers
         action = input(">")
         if "jump" in action.lower():
             print(dedent("""
@@ -81,6 +93,8 @@ class Car(Scene):
                 to give up.
                 """))
             return 'lost'
+        # If none of the common answers to included in the user input,
+        # ask to try again and replay the scene
         else:
             print("Invalid input.  Please try something else.")
             return 'car'
@@ -94,6 +108,7 @@ class ParkingLot(Scene):
         a blue SUV signal simultaneously. This could get nasty...
         """))
 
+        # Get input from user, compare to common answers
         action = input(">")
         if "let" in action.lower():
             print(dedent("""
@@ -141,6 +156,8 @@ class ParkingLot(Scene):
                 """))
             return 'lost'
         else:
+            # If none of the common answers to included in the user input,
+            # ask to try again and replay the scene
             print("Invalid input.  Please try something else.")
             return 'parking_lot'
 
@@ -154,9 +171,12 @@ class Fridge(Scene):
         two out of three.
         """))
 
+        # Initialize rock/paper/scissors wins counters
         your_wins = 0
         lady_wins = 0
 
+        # Loop through the rock/paper/scissors game until someone
+        # gets 3 wins
         while your_wins < 2 and lady_wins < 2:
             lady_move_int = randint(1,3)
             if lady_move_int == 1:
@@ -181,7 +201,9 @@ class Fridge(Scene):
                     your_move = "scissors"
                     break
                 else:
+                    # If input does not match a valid entry, ask to try again
                     print("Invalid Input.  Please enter 'rock', 'paper' or 'scissors'.")
+            # Increment wins for someone when they win a round
             if lady_move == your_move:
                 text = "It's a draw."
             elif lady_move == "rock":
@@ -207,9 +229,11 @@ class Fridge(Scene):
                     text = "Lady wins."
             else:
                 text = "It's a draw."
+            # Print the results of the round
             print(f"\nThe lady used {lady_move}, you used {your_move}. {text}")
             print(f"Lady: {lady_wins}   You: {your_wins}")
 
+        # Outcome if you reach 2 wins before the old lady
         if your_wins == 2:
             print(dedent("""
                 Incredibly, you won the match of rock/paper/scissors!
@@ -217,11 +241,13 @@ class Fridge(Scene):
                 the cashier.
                 """))
             return 'cashier'
+        # Outcome if the old lady reaches 2 wins before you
         else:
             print(dedent("""
                 Unfortunately, you were no match for the old lady.
                 Try again?
                 """))
+            # Give the player a chance to try the scene again
             action = input(">")
             if "y" in action:
                 return 'fridge'
@@ -238,6 +264,7 @@ class Cashier(Scene):
             quickly or the entire carton will end up on the floor.
             """))
 
+        # Get input from user, compare to common answers
         action = input(">")
         if "tape" in action.lower():
             print(dedent("""
@@ -266,6 +293,8 @@ class Cashier(Scene):
             However, now you have no milk for tomorrow...
             """))
             return 'lost'
+        # If none of the common answers to included in the user input,
+        # the game is over
         else:
             print(dedent("""
             You fumble around trying keep the milk in the leaking carton.
@@ -278,6 +307,7 @@ class Cashier(Scene):
 
 class Lost(Scene):
 
+    # List of print statements for game over to be played randomly
     quips = [
         "You lost. Maybe this is too hard for you",
         "You couldn't even buy milk. Where did it all go wrong?",
@@ -286,20 +316,24 @@ class Lost(Scene):
         "No milk for you. What did you do to deserve this?"
     ]
 
+    # Prints a random quip plus GAME OVER
     def enter(self):
         print(Lost.quips[randint(0, len(self.quips) - 1)],"\n\nGAME OVER")
         exit(1)
 
 class Finished(Scene):
 
+    # Victory print statement
     def enter(self):
         print(dedent("""
             You have successfully bought milk. Good job!
             """))
         exit(0)
 
+# Map class to navigate between scenes
 class Map(object):
 
+    # Scenes dictionary by return string to class name
     scenes = {
         'car': Car(),
         'parking_lot': ParkingLot(),
@@ -309,12 +343,15 @@ class Map(object):
         'finished': Finished()
     }
 
+    # Init method initializes the start_scene String
     def __init__(self, start_scene):
         self.start_scene = start_scene
 
+    # Obtain the next_scene Class name from the return String entered
     def next_scene(self, scene_name):
         val = Map.scenes.get(scene_name)
         return val
 
+    # Get the opening scene by Class name
     def opening_scene(self):
         return self.next_scene(self.start_scene)
